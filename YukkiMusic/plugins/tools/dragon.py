d52efb4cf06e4802107a79369c3515f6dec7bf8b
dragon-ka <111325414+dragon-ka@users.noreply.github.com>
2022-08-18 17:59:05 +0300

    Update dragon.py

--- YukkiMusic/plugins/tools/dragon.py
+++ YukkiMusic/plugins/tools/dragon.py
@@ -84,7 +84,7 @@ async def khalid(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""نـيـمـك{message.from_user.mention}\n\nيـوزرك@{message.from_user.username}\n\nالايدي {message.from_user.id}\n\nايـدي الـجـروب{message.chat.id}""", 
+                    await message.reply_photo(photo.file_id,       caption=f"""نـيـمـك{message.from_user.mention}\n\nيـوزرك @{message.from_user.username}\n\nالايدي {message.from_user.id}\n\nايـدي الـجـروب{message.chat.id}""", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
