ca09de414bb613b4a24eef99cffd42d978e81410
dragon-ka <111325414+dragon-ka@users.noreply.github.com>
2022-08-24 16:15:18 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -39,24 +39,6 @@ from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
 
-force_btn = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                text="𝑫𝒓𝒂𝒈𝒐𝒏 𝑺𝒐𝒖𝒓𝒄𝒆", url="https://t.me/yy8gg"
-            ),                        
-        ],        
-    ]
-)
-
-async def check_is_joined(message):    
-    try:
-        userid = message.from_user.id
-        status = await app.get_chat_member("yy8gg", userid)
-        return True
-    except Exception:
-        await message.reply_text("**• عذࢪا عݪيك الأشتࢪاك بقناة اݪبوت أولا :**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
-        return False
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
