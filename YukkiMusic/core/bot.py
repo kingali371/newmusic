5d9910f0a87ce93e1cb33eaec6d75131e6e04f72
احمد <44296110+SASKYBO@users.noreply.github.com>
2022-08-26 17:09:46 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -18,7 +18,7 @@ from ..logging import LOGGER
 
 class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"بدا التنصيب")
         super().__init__(
             "YukkiMusicBot",
             api_id=config.API_ID,
@@ -33,21 +33,21 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "تم تنصيب سورس دراكون بنجاح"
+                config.LOG_GROUP_ID, "تم تنصيب السورس بنجاح"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "فشل الروبوت في الوصول إلى مجموعة السجل. تأكد من أنك أضفت الروبوت الخاص بك إلى قناة السجل الخاصة بك وقمت بالترقية كمسؤول!"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "يرجى ترقية البوت ادمن في المجموعة"
             )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"لقد تم التشغيل بنجاح {self.name}")
